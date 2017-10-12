11e0dc2afc3ca0fd4dc4f894bf888cf10002227b
alexisvt <alexisvt@gmail.com>
2017-10-11 20:49:13 -0600

    adding text

--- project-managment/src/components/About.vue
+++ project-managment/src/components/About.vue
@@ -9,7 +9,7 @@ export default {
   name: 'About',
   data() {
     return {
-      msg: 'About me page :D'
+      msg: 'About me page :D, this message is dynamic injected into the page'
     };
   }
 };
