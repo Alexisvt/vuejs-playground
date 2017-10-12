634e1c252437b1ce6b6313d8a18b15b9117b49af
alexisvt <alexisvt@gmail.com>
2017-10-11 20:41:06 -0600

    playing routing

---
+++ project-managment/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="About">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      msg: 'About me page :D'
+    };
+  }
+};
+</script>
--- project-managment/src/router/index.js
+++ project-managment/src/router/index.js
@@ -1,3 +1,4 @@
+import About from '@/components/About';
 import HelloWorld from '@/components/HelloWorld';
 import Vue from 'vue';
 import Router from 'vue-router';
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
     }
   ]
 });
